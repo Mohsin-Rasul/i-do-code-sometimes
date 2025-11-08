372a6c30e3406ce5dbc80deb16882b322232a70c
Mohsin Rasul <mohsinrasul80@gmail.com>
2025-11-08 23:59:32 +0500

    Create my.py

---
+++ Lab 5/my.py
@@ -0,0 +1,12 @@
+stack=[]
+def push():
+    stack.append("item")
+    stack.append("kjds")
+    stack.append("ssd")
+    stack.append("sdlkn")
+    stack.append("djd")
+
+push()
+
+stack.reverse()
+print(stack)
